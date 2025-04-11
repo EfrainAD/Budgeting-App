ff933205fb44847b318099c935ec57d805392764
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2025-04-10 20:50:03 -0400

    Add totel amount and a way to edit it's value when you click button

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Text, View, Button, TextInput, SafeAreaView, TouchableOpacity } from "react-native";
 
 export default function Index() {
+  const [total, setTotal] = useState(0)
+  const [showTotal, setShowTotal] = useState(false)
+
   return (
     <SafeAreaView
       style={{
@@ -9,7 +13,25 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={{ flexDirection: 'row' }} >
+        <Text>Total: ${total}</Text>
+
+        <TouchableOpacity onPress={() => { setShowTotal(!showTotal) }}><Text style={{ paddingLeft: 50 }}>{showTotal ? "Done" : "Change Total"}</Text></TouchableOpacity>
+      </View>
+      {
+        showTotal &&
+        <TextInput
+          style={{
+            height: 40,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+          }}
+          value={total.toString()}
+          onChangeText={(e) => setTotal(Number(e))}
+          keyboardType="numeric"
+        />
+      }
     </SafeAreaView>
   );
 }
