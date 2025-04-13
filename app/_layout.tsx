8e6de7a18a5de6d395627be7bc9508c589ad7065
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2025-04-13 13:49:52 -0400

    Clean Up - Remove a commented out code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  // return <Stack options={{ headerShown: false }} />;
   return <Stack screenOptions={{ headerShown: false }} />;
 }
