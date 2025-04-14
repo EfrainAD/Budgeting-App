9c51db838e2dd8a9a4681b45782ceed242af1f22
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2025-04-14 15:50:39 -0400

    Add CRUD functions in storage.js for spending section and planned spending

--- services/storage.js
+++ services/storage.js
@@ -1,8 +1,19 @@
-import { createDefaultIncome } from '../constants/defaults.js'
-import { fakeIncomes } from '../constants/fakeData.js'
+import {
+   createDefaultIncome,
+   createDefaultSpending,
+   createDefaultSection,
+} from '../constants/defaults.js'
+import {
+   fakeIncomes,
+   fakeSections,
+   fakeSpending,
+} from '../constants/fakeData.js'
 
 let incomes = fakeIncomes
+let sections = fakeSections
+let spendings = fakeSpending
 
+// INCOME
 export const getIncomes = () => incomes
 
 export const addIncome = () => {
@@ -20,4 +31,44 @@ export const editIncome = (editedIncome) => {
 export const deleteIncome = (id) => {
    incomes = incomes.filter((income) => income.id !== id)
    return incomes
+}
+
+// SECTION
+export const getSections = () => sections
+
+export const addSection = () => {
+   sections.push(createDefaultSection())
+   return [...sections]
+}
+
+export const editSection = (editedSection) => {
+   sections = sections.map((section) =>
+      section.id === editedSection.id ? editedSection : section
+   )
+   return sections
+}
+
+export const deleteSection = (id) => {
+   sections = sections.filter((section) => section.id !== id)
+   return sections
+}
+
+///// Spending
+export const getSpendings = () => spendings
+
+export const addSpending = (section_id) => {
+   spendings.push(createDefaultSpending(section_id))
+   return [...spendings]
+}
+
+export const editSpending = (editedSpending) => {
+   spendings = spendings.map((spending) =>
+      spending.id === editedSpending.id ? editedSpending : spending
+   )
+   return spendings
+}
+
+export const deleteSpending = (id) => {
+   spendings = spendings.filter((spending) => spending.id !== id)
+   return spendings
 }
